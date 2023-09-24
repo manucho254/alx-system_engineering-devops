54951331fbb32e011b8cd4b94190e24f41024a4e
manucho254 <manuchoadero@gmail.com>
2023-09-24 19:13:05 +0000

    client configuration puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -9,3 +9,9 @@ file_line {'Setup identity':
     path   => '/etc/ssh/ssh_config',
     line   => '   IdentityFile  ~/.ssh/school',
 }
+
+file_line {'Setup identity':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '    PasswordAuthentication no',
+}
