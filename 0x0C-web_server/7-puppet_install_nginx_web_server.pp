3b8aace572f1517104d2ada994fc9a3b72fbfcc1
manucho254 <manuchodero@gmail.com>
2023-10-02 20:08:11 +0000

    fixes to puupet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -39,27 +39,27 @@ file { '/etc/nginx/sites-enabled/default':
         path    => '/etc/nginx/sites-enabled/default',
         content =>
 'server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-    root /var/www/html;
+        root /var/www/html;
 
-    index index.html index.htm index.nginx-debian.html;
+        index index.html index.htm index.nginx-debian.html;
 
-    server_name _;
+        server_name _;
 
-    # 404 error file
-    error_page 404 /error404.html;
+        # 404 error file
+        error_page 404 /error404.html;
 
-    location / {
-             # First attempt to serve request as file, then
-             # as directory, then fall back to displaying a 404.
-             try_files $uri $uri/ =404;
-    }
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files $uri $uri/ =404;
+        }
 
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
 }
 ',
 }
@@ -71,28 +71,28 @@ file { '/etc/nginx/sites-available/default':
         path    => '/etc/nginx/sites-available/default',
         content =>
 'server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-    root /var/www/html;
+        root /var/www/html;
 
-    index index.html index.htm index.nginx-debian.html;
+        index index.html index.htm index.nginx-debian.html;
     
-    server_name _;
+        server_name _;
 
-    # 404 error file
-    error_page 404 /error404.html;
+        # 404 error file
+        error_page 404 /error404.html;
     
-    location / {
-             # First attempt to serve request as file, then
-             # as directory, then fall back to displaying a 404.
-             try_files $uri $uri/ =404;
-    }
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files $uri $uri/ =404;
+        }
 
-    # redirect user
-    location /redirect_me {
-         return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
+        # redirect user
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
 }
 ',
 }
