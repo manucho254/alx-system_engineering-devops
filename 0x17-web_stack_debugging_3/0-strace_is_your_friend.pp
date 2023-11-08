5c8ab066721f354d9a11f2cb747647079491f600
manucho254 <manuchoadero@gmail.com>
2023-11-08 19:48:26 +0000

    fix error in apache2

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,10 @@
 # Puppet script to fix Apache, that is returning a 500 error.
 
+exec{'start mysql':
+    command => 'sudo service mysql restart',
+    path    => ['/usr/bin', '/usr/sbin',],
+}
+
 exec{'fix wordpress misconfig':
     command => 'sed -i "s/phpp/php/g" var/www/html/wp-settings.php',
     path    => ['/usr/bin', '/usr/sbin',],
