8a0532ef84761879122ed5665e97e5d9e99fd315
manucho254 <manuchoadero@gmail.com>
2023-11-08 16:30:59 +0000

    setup a dashboard in datadog

--- 0x18-webstack_monitoring/test_datadog.py
+++ 0x18-webstack_monitoring/test_datadog.py
@@ -2,6 +2,7 @@
 
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v1.api.hosts_api import HostsApi
+from datadog_api_client.v1.api.dashboards_api import DashboardsApi
 
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
@@ -10,4 +11,14 @@ with ApiClient(configuration) as api_client:
             filter="env:ci",
             )
 
+    print(response)
+
+print("dashboards")
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = DashboardsApi(api_client)
+    response = api_instance.list_dashboards(
+            filter_shared=False,
+            )
     print(response)
