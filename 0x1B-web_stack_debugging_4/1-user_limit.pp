e6a857639236629b591cd297bd28599f99af25e2
manucho254 <manuchoadero@gmail.com>
2023-11-16 19:35:42 +0000

    fixes to puppet

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -4,3 +4,15 @@ exec {'change-os-configuration-for-holberton-user':
   command => 'sudo useradd -m holberton',
   path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
 }
+
+# Increase hard file limit for user holberton
+exec { 'increase hard file limit':
+  command => 'sed -i "/holberton hard/s/4/100000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for user holberton
+exec { 'increase sof file limit':
+  command => 'sed -i "/holberton soft/s/5/100000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
