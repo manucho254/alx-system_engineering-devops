8bf114f1a2d0446af991aaaf7ff053a64c665731
manucho254 <manuchoadero@gmail.com>
2023-11-16 17:20:26 +0000

    fixes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,3 +4,8 @@ exec {'fix--for-nginx':
   command => 'sudo sed -i "s/nginx.pid;/nginx.pid;\nworker_rlimit_nofile 300000;/" /etc/nginx/nginx.conf',
   path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
 }
+
+exec {'Restart nginx':
+  command => 'sudo service nginx restart',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
+}
