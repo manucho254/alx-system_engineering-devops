09dafe7f94b333516144b6bbbbed065692746e10
manucho254 <manuchoadero@gmail.com>
2023-11-16 19:32:16 +0000

    fixes to puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,16 +5,6 @@ exec {'update file max':
   path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
 
-exec {'update limits':
-  command => 'sudo echo -e "nginx       soft    nofile   10000\nnginx       hard    nofile  300000" | tee -a /etc/security/limits.conf',
-  path    => ['/usr/bin', '/usr/sbin', '/bin'],
-}
-
-exec {'update limits':
-  command => 'sudo sysctl -p',
-  path    => ['/usr/bin', '/usr/sbin', '/bin'],
-}
-
 exec { 'fix--for-nginx':
   command => '/bin/sed -i "s/15/10000/" /etc/default/nginx',
   path    => ['/usr/bin', '/usr/sbin', '/bin'],
