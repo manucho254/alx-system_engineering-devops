a0df6ca4c601f061f3a6be57292ba5503544b379
manucho254 <manuchoadero@gmail.com>
2023-11-16 19:23:30 +0000

    fixes to puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,26 +1,26 @@
 # fix Too many open files error nginx
 
 exec {'fix--for-nginx':
-  command  => 'sudo sed -i "s/nginx.pid;/nginx.pid;\nworker_rlimit_nofile 300000;/" /etc/nginx/nginx.conf',
-  path     => ['/usr/bin', '/usr/sbin', '/bin'],
+  command => 'sudo sed -i "s/nginx.pid;/nginx.pid;\nworker_rlimit_nofile 300000;/" /etc/nginx/nginx.conf',
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
 
 exec {'update file max':
-  command  =>  'sudo echo -e "fs.file-max = 100000" | tee -a /etc/sysctl.conf',
-  path     => ['/usr/bin', '/usr/sbin', '/bin'],
+  command =>  'sudo echo -e "fs.file-max = 100000" | tee -a /etc/sysctl.conf',
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
 
 exec {'update limits':
-  command  => 'sudo echo -e "nginx       soft    nofile   10000\nnginx       hard    nofile  300000" | tee -a /etc/security/limits.conf',
-  path     => ['/usr/bin', '/usr/sbin', '/bin'],
+  command => 'sudo echo -e "nginx       soft    nofile   10000\nnginx       hard    nofile  300000" | tee -a /etc/security/limits.conf',
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
 
 exec {'update limits':
-  command  => 'sudo sysctl -p',
-  path     => ['/usr/bin', '/usr/sbin', '/bin'],
+  command => 'sudo sysctl -p',
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
 
 exec {'Restart nginx':
-  command  => 'sudo service nginx restart',
-  path     => ['/usr/bin', '/usr/sbin', '/bin'],
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
