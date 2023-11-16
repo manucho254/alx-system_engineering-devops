d103c3c77b40599c965289b0e509b7f7c42e0be1
manucho254 <manuchoadero@gmail.com>
2023-11-16 19:28:43 +0000

    fixes to puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,5 @@
 # fix Too many open files error nginx
-
-exec {'fix--for-nginx':
-  command => 'sudo sed -i "s/nginx.pid;/nginx.pid;\nworker_rlimit_nofile 300000;/" /etc/nginx/nginx.conf',
-  path    => ['/usr/bin', '/usr/sbin', '/bin'],
-}
-
+#
 exec {'update file max':
   command =>  'sudo echo -e "fs.file-max = 100000" | tee -a /etc/sysctl.conf',
   path    => ['/usr/bin', '/usr/sbin', '/bin'],
@@ -17,6 +12,11 @@ exec {'update limits':
 
 exec {'update limits':
   command => 'sudo sysctl -p',
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
+}
+
+exec { 'fix--for-nginx':
+  command => '/bin/sed -i "s/15/10000/" /etc/default/nginx',
   path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
 
